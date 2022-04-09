628fa0d39c3fcef14810cf8673f21f842462be31
Sherzod7709 <eshnazarov044@gmail.com>
2022-04-09 23:59:34 +0500

    finish

--- index.js
+++ index.js
@@ -5,17 +5,27 @@ var GUESS_NUMBER_MIN=1;
 var confirmed=confirm ("Salom omadingizni sinab ko'rasizmi")
 console.log(confirmed);
 if (confirmed) {
-    var RANDOM_NUMBER = Math.floor(Math.random() * (GUESS_NUMBER_MAX - GUESS_NUMBER_MAX + 1) + GUESS_NUMBER_MIN);
+    var RANDOM_NUMBER = Math.floor(Math.random() * (GUESS_NUMBER_MAX - GUESS_NUMBER_MIN + 1) + GUESS_NUMBER_MIN);
     var number =prompt ("son kiriting agar random bilan mos tushsa tabriklaymiz (sizda 3 ta urinish mavjud)")
     while (count<3) {
         if (number!=RANDOM_NUMBER) {
-            var number=prompt ("qayta urinib ko'ring ")
+            if (number > RANDOM_NUMBER) {
+                alert ("Random kiritilgan sondan kichkina")
+                var number=prompt ("qayta urinib ko'ring ")
             count++;
+            }
+            else if (number < RANDOM_NUMBER) {
+                alert ("Random kiritilgan sondan katta")
+                var number=prompt ("qayta urinib ko'ring ")
+            count++;
+            }
         }
         if (number==RANDOM_NUMBER) {
             alert ("Qoyil topdingiz")
             count=3;
         }
     }
-    alert ("Kechirasiz boshqa safar urinib ko'rasiz")
+    if (number!=RANDOM_NUMBER) {
+        alert ("Omadingizni boshqa safar sinab ko'ring")
+    }
 }
